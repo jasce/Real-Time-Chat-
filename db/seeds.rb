e069c4af47731d5d8c5b236e42027482372d3002
Jasmeet Singh <jascee.gtbit@gmail.com>
2016-11-23 12:03:09 +0530

    User model and authentication added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+password = '12345678'
+
+1.upto(5) do |i| 
+	User.create(
+	email: "user-#{i}@example.com",
+	password: password,
+	password_confirmation: password
+	)
+end
