1ea7b95c53188a636239f8e9d84722713b28a51f
Jasmeet Singh <jascee.gtbit@gmail.com>
2016-12-23 21:49:44 +0530

    Added Front end Conversation Channel

--- app/assets/javascripts/channels/conversation.js
+++ app/assets/javascripts/channels/conversation.js
@@ -9,9 +9,18 @@ App.conversation = App.cable.subscriptions.create("ConversationChannel", {
 
   received: function(data) {
     // Called when there's incoming data on the websocket for this channel
+    console.log(data['message']);
   },
 
   speak: function() {
-    return this.perform('speak');
+    return this.perform('speak', {
+      message: message
+    });
   }
 });
+$(document).on('submit', '.new_message', function(e){
+  e.preventDefault();
+  var values = $(this).serializeArray();
+  App.conversation.speak(values);
+  $(this).trigger('reset');
+})
