14312a27fd012ebeb48e2b5e15e27d51be5f491b
Jasmeet Singh <jascee.gtbit@gmail.com>
2017-01-06 18:09:10 +0530

    Almost done

--- app/assets/javascripts/channels/conversation.js
+++ app/assets/javascripts/channels/conversation.js
@@ -1,27 +1,43 @@
 App.conversation = App.cable.subscriptions.create("ConversationChannel", {
-  connected: function() {
-    // Called when the subscription is ready for use on the server
-  },
-
-  disconnected: function() {
-    // Called when the subscription has been terminated by the server
-  },
-
+  connected: function() {},
+  disconnected: function() {},
   received: function(data) {
-    // Called when there's incoming data on the websocket for this channel
-    console.log(data['message']);
-  },
+    var conversation = $('#conversations-list').find("[data-conversation-id='" + data['conversation_id'] + "']");
 
-  speak: function(message) {    
-    
-    return this.perform('speak', {      
+    if (data['window'] !== undefined) {
+      var conversation_visible = conversation.is(':visible');
+
+      if (conversation_visible) {
+        var messages_visible = (conversation).find('.panel-body').is(':visible');
+
+        if (!messages_visible) {
+          conversation.removeClass('panel-default').addClass('panel-success');
+        }
+        conversation.find('.messages-list').find('ul').append(data['message']);
+      }
+      else {
+        $('#conversations-list').append(data['window']);
+        conversation = $('#conversations-list').find("[data-conversation-id='" + data['conversation_id'] + "']");
+        conversation.find('.panel-body').toggle();
+      }
+    }
+    else {
+      conversation.find('ul').append(data['message']);
+    }
+
+    var messages_list = conversation.find('.messages-list');
+    var height = messages_list[0].scrollHeight;
+    messages_list.scrollTop(height);
+  },
+  speak: function(message) {
+    return this.perform('speak', {
       message: message
     });
   }
 });
-$(document).on('submit', '.new_message', function(e){
+$(document).on('submit', '.new_message', function(e) {
   e.preventDefault();
-  var values = $(this).serializeArray(); 
-  App.conversation.speak(values); 
+  var values = $(this).serializeArray();
+  App.conversation.speak(values);
   $(this).trigger('reset');
 });
